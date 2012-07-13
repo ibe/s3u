38546116f9372c91102e726acf2c90dd3d2ec097
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-07-13 09:01:44 +0200

    added trial_id for seeding

--- eligibilitycheck/app/models/patient.rb
+++ eligibilitycheck/app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ActiveRecord::Base
   has_many :medical_cases
-  attr_accessible :extId, :prename, :surname, :dob, :sex, :extDocId, :consent_status, :self_service_status
+  attr_accessible :extId, :prename, :surname, :dob, :sex, :extDocId, :consent_status, :self_service_status, :trial_id
   
   def trial
     Trial.find(self.trial_id)
