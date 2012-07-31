3b79562621e84fe35ceae8fcab906fcfefb1d05e
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-07-31 16:05:41 +0200

    fixes #15

---
+++ hl7interface/perl_service/cron.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 52 15 * * * /bin/bash /home/HELIOS/tschlein/github/s3u/hl7interface/perl_service/cron.sh
+
+cd /home/HELIOS/tschlein/github/s3u/hl7interface/perl_service
+/usr/bin/perl /home/HELIOS/tschlein/github/s3u/hl7interface/perl_service/s3u_lmu_eraser.pl
