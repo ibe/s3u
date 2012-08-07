db9910fb7035566760cf8411ebb93569b2e6e5a6
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-07 13:54:21 +0200

    addresses #27

--- trialregistry/app/models/trial.rb
+++ trialregistry/app/models/trial.rb
@@ -4,5 +4,8 @@ class Trial < ActiveRecord::Base
   #has_many :subjects
   attr_accessible :extId, :description, :recruitingTarget, :recruiting_status, :prenameInvestigator, :surnameInvestigator, :mailInvestigator, :informed_consent
   
-  has_attached_file :informed_consent
+  # deployed to sub uri ("http://foo.bar/trialregistry" instead of "http://foo.bar")
+  # therefor having to workaround bug in paperclip
+  # see https://github.com/thoughtbot/paperclip/issues/889
+  has_attached_file :informed_consent, :url => "/trialregistry/system/:class/:attachment/:id/:style/:filename"
 end
