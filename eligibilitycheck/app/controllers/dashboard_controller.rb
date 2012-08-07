d51ddc579cc868821625457e6b2229bb0d95c22e
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-07 13:38:38 +0200

    addresses #22

--- eligibilitycheck/app/controllers/dashboard_controller.rb
+++ eligibilitycheck/app/controllers/dashboard_controller.rb
@@ -5,8 +5,9 @@ class DashboardController < ApplicationController
   # GET /dashboard
   # GET /dashboard.json
   def index
-    @total_patients = Patient.where(:extDocId => current_user.extDocId)
-    @new_patients = @total_patients.where("created_at > ?", current_user.last_sign_in_at)
+    @total_patients = Patient.where("extDocId = ? AND consent_status IS NULL", current_user.extDocId)
+    @new_patients = @total_patients.where("created_at > ?", current_user.current_sign_in_at.in_time_zone('Berlin'))
+    @remaining_patients = @total_patients.count - @new_patients.count
 
     respond_to do |format|
       format.html # index.html.erb
