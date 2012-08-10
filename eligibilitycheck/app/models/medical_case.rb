b59a2791fd020ba5da41ce9224ec52d9fc03ee2c
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-10 10:05:18 +0200

    addresses #9

--- eligibilitycheck/app/models/medical_case.rb
+++ eligibilitycheck/app/models/medical_case.rb
@@ -2,5 +2,5 @@ class MedicalCase < ActiveRecord::Base
   belongs_to :patient
   has_many :diagnoses, :dependent => :destroy
   attr_accessible :extCaseId, :funcOu, :nurseOu, :patient_id
-  paginates_per 14
+  paginates_per 12
 end
