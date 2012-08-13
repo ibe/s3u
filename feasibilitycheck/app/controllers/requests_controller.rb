a287b7e0567abd919bf0022d1adf86a94c7c991f
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-13 15:14:47 +0200

    addresses #7 and #16

--- feasibilitycheck/app/controllers/requests_controller.rb
+++ feasibilitycheck/app/controllers/requests_controller.rb
@@ -47,6 +47,8 @@ class RequestsController < ApplicationController
 
     respond_to do |format|
       if @request.save
+        RequestMailer.welcome_email(@request).deliver
+        
         format.html { redirect_to @request, :notice => 'Request was successfully created.' }
         format.json { render :json => @request, :status => :created, :location => @request }
       else
