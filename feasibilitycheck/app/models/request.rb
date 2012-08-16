547386171251016fb243e1231a53d2a1b1513424
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-16 13:11:59 +0200

    addresses #32

--- feasibilitycheck/app/models/request.rb
+++ feasibilitycheck/app/models/request.rb
@@ -2,4 +2,5 @@ class Request < ActiveRecord::Base
   has_many :criteria
   attr_accessible :prenameContact, :surnameContact, :mailContact, :phoneContact, :description, :approved, :submit
   paginates_per 7
+  validates :prenameContact, :surnameContact, :mailContact, :phoneContact, :description, :presence => true
 end
