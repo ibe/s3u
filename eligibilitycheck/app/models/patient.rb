4abfd16a6d50e460e19689e7b2478e6b45eb12f2
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-27 07:26:22 +0200

    addresses #26, #12 and #32

--- eligibilitycheck/app/models/patient.rb
+++ eligibilitycheck/app/models/patient.rb
@@ -1,25 +1,30 @@
 class Patient < ActiveRecord::Base
   has_many :medical_cases, :dependent => :destroy
-  attr_accessible :extId, :prename, :surname, :dob, :sex, :extDocId, :consent_status, :self_service_status, :trial_id
+  attr_accessible :extId, :prename, :surname, :dob, :sex, :extDocId, :consent_status, :self_service_status, :trial_id, :read_status
   paginates_per 7
 
-  validates :extId, :prename, :surname, :extDocId, :consent_status, :self_service_status, :presence => true
+  validates :extId, :prename, :surname, :extDocId, :presence => true
   validates :sex, :inclusion => { :in => %w(M F) }
   validates :dob, :format => { :with => /[0-9]{4}-[0-9]{2}-[0-9]{2}/ }
   
-  after_save :update_studienmonitor
+  after_save :update_remote_apps
   
   def trial
     Trial.find(self.trial_id)
   end
 
-  def update_studienmonitor
+  def update_remote_apps
     @trial = self.trial
     if self.consent_status == 1
       @trial.update_attributes(:recruiting_status => @trial.recruiting_status + 1)
-    else
+      @cdms_subject = CdmsSubject.new(:prename => self.prename, :surname => self.surname)
+      if @cdms_subject.new?
+        @cdms_subject.save!
+      else
+        @cdms_subject.update   
+      end
+    elsif self.consent_status == 0
       @trial.update_attributes(:recruiting_status => @trial.recruiting_status - 1)
     end
   end
-
 end
