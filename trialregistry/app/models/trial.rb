d056d482319b96a7f88812fe1a7cd040db4513c5
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-08-27 07:35:52 +0200

    addresses #26

--- trialregistry/app/models/trial.rb
+++ trialregistry/app/models/trial.rb
@@ -3,7 +3,9 @@ class Trial < ActiveRecord::Base
   #has_many :data, :through => :criteria
   #has_many :subjects
   attr_accessible :extId, :description, :recruitingTarget, :recruiting_status, :prenameInvestigator, :surnameInvestigator, :mailInvestigator, :informed_consent
-  
+  before_create do |trial|
+    trial.recruiting_status = 0
+  end
   # deployed to sub uri ("http://foo.bar/trialregistry" instead of "http://foo.bar")
   # therefor having to workaround bug in paperclip
   # see https://github.com/thoughtbot/paperclip/issues/889
