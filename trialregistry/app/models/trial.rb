0eca6dd621ba0d8de14a54af56c851e0e8c3571c
tobias-schleinkofer <tobias.schleinkofer@med.uni-muenchen.de>
2012-11-09 08:22:13 +0100

    addresses #49 and #32

--- trialregistry/app/models/trial.rb
+++ trialregistry/app/models/trial.rb
@@ -3,10 +3,13 @@ class Trial < ActiveRecord::Base
   has_many :hits
   #has_many :data, :through => :criteria
   #has_many :subjects
-  attr_accessible :extId, :description, :recruitingTarget, :recruiting_status, :prenameInvestigator, :surnameInvestigator, :mailInvestigator, :informed_consent
+  attr_accessible :extId, :description, :recruitingTarget, :recruiting_status, :prenameInvestigator, :surnameInvestigator, :mailInvestigator, :informed_consent, :activated
   before_create do |trial|
     trial.recruiting_status = 0
   end
+  
+  validates :extId, :description, :presence => true
+  
   # deployed to sub uri ("http://foo.bar/trialregistry" instead of "http://foo.bar")
   # therefor having to workaround bug in paperclip
   # see https://github.com/thoughtbot/paperclip/issues/889
